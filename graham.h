362b6cd30f8d9397b061229cb70c0e7b72ec3fbb
Eowyn <eowyn_mangahas@dlsu.edu.ph>
2026-06-22 00:06:59 +0800

    Update graham.h

--- graham.h
+++ graham.h
@@ -1,8 +1,11 @@
 #ifndef GRAHAM_H
 #define GRAHAM_H
- 
-#include "stack.h"
-#define MAX 32768
 
-//functions here
-//feel free to edit the include
+#include "stack.h"
+
+#define MAX_POINTS 32768  
+
+void graham_scan_slow(Point *pts, int n, Stack *hull);
+void graham_scan_fast(Point *pts, int n, Stack *hull); /*will edit depending on code!*/
+
+#endif 
