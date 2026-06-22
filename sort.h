e6d34eeed11ad5b235e2cc6d19a9ca8102abcf5f
Eowyn <eowyn_mangahas@dlsu.edu.ph>
2026-06-23 02:14:58 +0800

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,8 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
- 
-#include "stack.h"
 
+#include "stack.h"  
+double cross_product(Point O, Point A, Point B);
+double dist_sq(Point a, Point b);
+void slow_sort(Point *pts, int n, Point anchor);
+void fast_sort(Point *pts, int n, Point anchor);
 
-//functions here
-//feel free to edit the include
+#endif  
